01cf824c2d738120095d2d0034a60ce67df7f84d
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2023-03-31 02:10:40 +0600

    club Banner , Registration , blogs done

--- src/Pages/Users/CLub&Forum/ClubForum.js
+++ src/Pages/Users/CLub&Forum/ClubForum.js
@@ -9,7 +9,7 @@ const ClubForum = () => {
                 <h1 className='text-3xl font-bold mb-20 text-secondary text-center'>Our Best Club & Forum Name</h1>
             </div>
             <marquee>
-                <div className='flex w-full justify-center'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 w-full justify-center'>
                     <div className='px-8 py-4 shadow-lg mr-6 border-t-4 border-[#64CEE6] rounded-md ml-10'>
                         <img className='w-44 h-28 mx-auto' src="https://the7.io/elementor-company/wp-content/uploads/sites/98/2022/06/lgcolor11.png" alt="" />
                     </div>
@@ -27,19 +27,7 @@ const ClubForum = () => {
                     </div>
                     <div className='px-8 py-4 shadow-lg mr-6 border-t-4 border-[#64CEE6] rounded-md ml-10'>
                         <img className='w-44 h-28 mx-auto' src="https://the7.io/elementor-company/wp-content/uploads/sites/98/2022/06/lgcolor11.png" alt="" />
-                    </div>
-                    <div className='px-8 py-4 shadow-lg mr-6 border-t-4 border-[#64CEE6] rounded-md ml-10'>
-                        <img className='w-44 h-28 mx-auto' src="https://the7.io/elementor-company/wp-content/uploads/sites/98/2022/06/lgcolor02.png" alt="" />
-                    </div>
-                    <div className='px-8 py-4 shadow-lg mr-6 border-t-4 border-[#64CEE6] rounded-md ml-10'>
-                        <img className='w-44 h-28 mx-auto' src="https://the7.io/elementor-company/wp-content/uploads/sites/98/2022/06/lgcolor03.png" alt="" />
-                    </div>
-                    <div className='px-8 py-4 shadow-lg mr-6 border-t-4 border-[#64CEE6] rounded-md ml-10'>
-                        <img className='w-44 h-28 mx-auto' src="https://i.ibb.co/3vz7qTb/Kiewit-Corporation-Logo-wine.png" alt="" />
-                    </div>
-                    <div className='px-8 py-4 shadow-lg mr-6 border-t-4 border-[#64CEE6] rounded-md ml-10'>
-                        <img className='w-44 h-28 mx-auto' src="https://i.ibb.co/L60h1b1/Booking-Holdings-Logo-wine.png" alt="" />
-                    </div>
+                    </div>  
                 </div>
             </marquee>
         </div>
