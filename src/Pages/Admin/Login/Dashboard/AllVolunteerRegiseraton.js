81dbfd70de5b22a3e6f3f362f0c70f5596f50881
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2023-04-12 23:55:16 +0600

    Event Register List Download as a PDF

--- src/Pages/Admin/Login/Dashboard/AllVolunteerRegiseraton.js
+++ src/Pages/Admin/Login/Dashboard/AllVolunteerRegiseraton.js
@@ -23,15 +23,17 @@ const AllVolunteerRegiseraton = () => {
       <div>
         <div>
           <div>
-            <h1 className="text-xl font-bold text-center mt-5">
+            <h1 className="text-xl font-bold text-center mt-5 mb-5">
               All Volunteer Request here
             </h1>
 
             <nav className="flex items-center  px-5 rounded-full lg:text-lg  font-bold justify-between max-w-2xl p-4 mx-auto">
-              <ul className="flex mx-auto  space-x-5  text-xs lg:text-lg font-bold ">
+              <ul className="grid grid-cols-2 mx-auto  space-x-5   text-xs lg:text-lg font-bold ">
                 {upcomingEvents.map((events) => (
-                  <li className="px-1 lg:px-2 py-2 hover:bg-primary hover:text-white  rounded-lg text-secondary">
-                    <CustomLink to={`/Dashboard/volunteerRequest/${events.title}`}>
+                  <li className="px-1 lg:px-2 py-2 shadow-xl  border-b-[1px] border-[#64CEE6] hover:bg-primary hover:text-white  rounded-lg text-secondary">
+                    <CustomLink
+                      to={`/Dashboard/volunteerRequest/${events.title}`}
+                    >
                       {events.title}
                     </CustomLink>
                   </li>
